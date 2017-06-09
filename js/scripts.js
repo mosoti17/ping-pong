47b108c946c3bd1d5e6a929a5717e6b1c93c8f5c
elvis <mosoti17@gmail.com>
2017-06-09 15:16:11 +0300

    separated business logic and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,24 @@
+function Pingpong(userInput) {
+  for(var i=1;i<=userInput;i++){
+    if (i % 15=== 0){
+      $("#answer").append('<li>'+"Pingpong"+'</li>');
+    }else if(i % 5 === 0){
+      $("#answer").append('<li>'+"Pong"+'</li>');
 
+    }else if(i % 3 === 0){
+      $("#answer").append('<li>'+"Ping"+'</li>');
+    }else {
+      $("#answer").append('<li>'+i+'</li>');
+    }
+  }  
+}
 
 $(document).ready(function () {
   $("form").submit(function (event) {
 
     var userInput = parseInt($("input#numbers").val());
-
+    var result = Pingpong(userInput)
 event.preventDefault();
-for(var i=1;i<=userInput;i++){
-  if (i % 15=== 0){
-    $("#answer").append('<li>'+"Pingpong"+'</li>');
-  }else if(i % 5 === 0){
-    $("#answer").append('<li>'+"Pong"+'</li>');
 
-  }else if(i % 3 === 0){
-    $("#answer").append('<li>'+"Ping"+'</li>');
-  }else {
-    $("#answer").append('<li>'+i+'</li>');
-  }
-}
     });
   });
