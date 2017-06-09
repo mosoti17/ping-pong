4cc08b080aaa2a3cc6747995f2b63195a1ce7c8c
elvis <mosoti17@gmail.com>
2017-06-09 15:01:11 +0300

    vlaues to be displayed in list

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,22 @@
-function pingpong(userInput) {
-  var results = [];
-  for(var i=1;i<=userInput;i++){
-    if (i % 15=== 0){
-      results.push("Pingpong");
-    }else if(i % 5 === 0){
-      results.push("pong")
 
-    }else if(i % 3 === 0){
-      results.push("ping")
-    }else {
-      results.push(i);
-    }
-}
- return results;
-}
+
 $(document).ready(function () {
   $("form").submit(function (event) {
 
     var userInput = parseInt($("input#numbers").val());
-    var result = pingpong(userInput);
-    result.forEach(function (output) {
 
 event.preventDefault();
+for(var i=1;i<=userInput;i++){
+  if (i % 15=== 0){
+    $("#answer").append('<li>'+"Pingpong"+'</li>');
+  }else if(i % 5 === 0){
+    $("#answer").append('<li>'+"Pong"+'</li>');
+
+  }else if(i % 3 === 0){
+    $("#answer").append('<li>'+"Ping"+'</li>');
+  }else {
+    $("#answer").append('<li>'+i+'</li>');
+  }
+}
     });
   });
-})
